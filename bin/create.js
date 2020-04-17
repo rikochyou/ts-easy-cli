1af6f7793b84d39c48766d2a972107833ad59356
riko <rikochyou@outlook.com>
2020-04-17 21:17:33 +0800

    修复default模板，增加更新提醒功能

--- bin/create.js
+++ bin/create.js
@@ -11,7 +11,9 @@ const chalk = require('chalk');
 const fs = require('fs');
 const { render } = require('consolidate').ejs;
 const Metalsmith = require('metalsmith');
-
+const updateNotifier = require('update-notifier');
+const pkg = require('../package.json');
+const boxen = require('boxen');
 DownloadGitRepo = promisify(DownloadGitRepo);
 
 const waitLoading = (message, fn) => async (...args) => {
@@ -38,10 +40,52 @@ const downloadTemplate = async (templateName) => {
   }
 };
 
-const installDependencies = async (projectName) => {
+const installDependencies = async (destPath) => {
+  shell.cd(destPath);
   await exec('npm install');
 };
+
+const checkVersion = () => {
+  const notifier = updateNotifier({
+    pkg,
+    updateCheckInterval: 0,
+  });
+
+  const { update } = notifier;
+  if (update) {
+    const message = [];
+    message.push(
+      `${chalk.bgYellowBright.black(' WARNI: ')} ${chalk.redBright(
+        'ts-easy-cli is not latest.'
+      )} \n`
+    );
+    message.push(
+      chalk.yellowBright('current ') +
+        chalk.yellowBright(update.current) +
+        chalk.yellowBright(' -> ') +
+        chalk.yellowBright('latest ') +
+        chalk.redBright(update.latest)
+    );
+    message.push(
+      `${chalk.blueBright('Up to date ')} ${chalk.redBright('npm i -g')} ${
+        pkg.name
+      }`
+    );
+
+    console.log(
+      boxen(message.join('\n'), {
+        padding: 2,
+        margin: 2,
+        align: 'center',
+        borderColor: 'yellow',
+        borderStyle: 'round',
+      })
+    );
+  }
+};
+
 module.exports = async (projectName) => {
+  checkVersion();
   const destPath = path.resolve(projectName);
   console.log(
     chalk.yellow('✨  Creating project in ') + chalk.cyan(`${destPath}`)
@@ -75,35 +119,41 @@ module.exports = async (projectName) => {
   const testPath = path.join(sourcePath, `test/${projectName}.test.ts`);
   shell.mv(srcTs, libPath);
   shell.mv(testTs, testPath);
+
   if (!fs.existsSync(askPath)) {
-    Metalsmith(__dirname)
-      .source(sourcePath)
-      .destination(destPath)
-      .use((files, metal, done) => {
-        const meta = metal.metadata();
-        const res = { name: projectName };
-        Object.assign(meta, res);
+    await new Promise((resolve, reject) => {
+      Metalsmith(__dirname)
+        .source(sourcePath)
+        .destination(destPath)
+        .use((files, metal, done) => {
+          const meta = metal.metadata();
+          const res = { name: projectName };
+          Object.assign(meta, res);
 
-        Reflect.ownKeys(files).forEach(async (filePath) => {
-          if (
-            filePath.includes('package.json') ||
-            filePath.includes('rollup')
-          ) {
-            const fileContent = files[filePath].contents.toString();
+          Reflect.ownKeys(files).forEach(async (filePath) => {
+            if (
+              filePath.includes('package.json') ||
+              filePath.includes('rollup')
+            ) {
+              const fileContent = files[filePath].contents.toString();
 
-            if (fileContent.includes('<%')) {
-              const resultContent = await render(fileContent, meta);
+              if (fileContent.includes('<%')) {
+                const resultContent = await render(fileContent, meta);
 
-              files[filePath].contents = Buffer.from(resultContent);
+                files[filePath].contents = Buffer.from(resultContent);
+              }
             }
+          });
+          done();
+        })
+        .build((err) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
           }
         });
-        done();
-      })
-      .build(function (err) {
-        if (err) throw err;
-        console.log('Build finished!');
-      });
+    });
   } else {
     await new Promise((resolve, reject) => {
       Metalsmith(__dirname)
@@ -149,17 +199,16 @@ module.exports = async (projectName) => {
         });
     });
   }
-
-  // 初始化本地git
-
   shell.cd(projectName);
+  // console.log('\n pwd git', shell.pwd());
   const rmaskPath = path.join(destPath, 'ask.js');
   rm('-rf', rmaskPath);
   const gitPath = path.join(destPath, '.git');
   rm('-rf', gitPath);
   await exec('git init');
+
   console.log(chalk.blueBright('✨  Initializing dependencies...'));
-  await waitLoading('install dependencies', installDependencies)(projectName);
+  await waitLoading('install dependencies', installDependencies)(destPath);
 
   console.log(
     chalk.yellowBright(' Successfully created project ') +
@@ -168,5 +217,5 @@ module.exports = async (projectName) => {
   console.log(chalk.yellowBright(' Get started with the following commands:'));
   console.log(chalk.blue(`$ cd ${projectName}`));
   console.log(chalk.blue('$ npm start'));
-  rm('-rf', sourcePath)
+  rm('-rf', sourcePath);
 };
